7898f083c04fbf1a57f8f982761568daf5403269
PRITVARMORA <pritjvarmora14044@gmail.com>
2020-09-24 19:25:37 +0530

    just test commints_no updates

--- Development_Code/Speed_Water_bot/Speed_Water_bot.ino
+++ Development_Code/Speed_Water_bot/Speed_Water_bot.ino
@@ -2,13 +2,13 @@
 #include <Servo.h>
 #define propeller  2//     engin start/stop    ==> pin D3
 #define led 4       ///notification led positive,negetive with d5
-#define flappin  6//       servo pin           ==> pin D2 
+#define flappin  6//       servo pin           ==> pin D2
 #define bltpown 9
-#define bltpowp 8       
+#define bltpowp 8
 uint8_t degree = 45;
 SoftwareSerial BTR (10,11);//(declared rx  ,   declared tx)
 Servo flap;
-
+//test comments____ignore this lines
 
 void setup() {
   flap.attach(flappin);
@@ -29,21 +29,21 @@ void setup() {
 void loop() {
  if (BTR.available())
  {
-  char data = BTR.read(); 
+  char data = BTR.read();
  // Serial.println(data);
  // Serial.println(data);
- 
+
   if     (data == '1')     {flap.write(degree - 32);/*Serial.println("left");*/}
-  
+
   else if(data == '2')     {flap.write(degree + 40);/*Serial.println("right");*/}
- 
+
   else if(data == '5')     {flap.write(degree);/*Serial.println("center");*/}
- 
+
   else if(data == '3')     {digitalWrite(propeller ,HIGH);digitalWrite(led ,HIGH);/*Serial.println("start");*/}
- 
+
   else if(data == '4')     { digitalWrite(propeller ,LOW);digitalWrite(led ,LOW);/*Serial.println("stop");*/}
-  
+
  }
-  
+
 
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Remote-Controlled-High-Speed-Water-Boat
-  How the project differs from other designs available in the market is its self designed creative architecture. The boat has specially optimized for racing competitions. It can be controlled from any smartphone connected by Bluetooth. Facing down position of BLDC thrusters in the air creates huge thrust with low power consumption and optimized to take sharp turns. 
+  How the project differs from other designs available in the market is its self designed creative architecture. The boat has specially optimized for racing competitions. It can be controlled from any smartphone connected by Bluetooth. Facing down position of BLDC thrusters in the air creates huge thrust with low power consumption and optimized to take sharp turns.
 # Folder: Delopment_Code
   This folder contains the Dveloped Arduino code for Speed Boat.
 # File: speed bot design photo.jpeg
-  The file is a photo of final implementation of the bot.
+  The file is a photo of final implementation of the bot....
